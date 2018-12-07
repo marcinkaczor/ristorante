5867c715a8d637131440ae289ff18c4cfd47ef28
marcinkaczor <marcin.kaczor96@gmail.com>
2018-12-07 21:55:02 +0100

    adding margin, fixing audits

--- src/components/CheckoutSummary/CheckoutSummary.js
+++ src/components/CheckoutSummary/CheckoutSummary.js
@@ -4,10 +4,10 @@ import Pizza from '../Pizza/Pizza';
 
 const checkoutSummary = (props) => {
   return (
-    <div className="mt-5">
+    <div className="my-5">
       <h3>We hope it tastes well!</h3>
       <Pizza ingredients={props.ingredients}/>
-      <div className="mt-5">
+      <div className="my-5">
         <Button color="secondary" onClick={props.checkoutCancelled}>Cancel</Button>{' '}
         <Button color="success" onClick={props.checkoutContinued}>Continue</Button>
       </div>
